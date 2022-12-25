936dc93f4813fdb11ae7c249396e9150cee60ae0
Jayasuriya1 <111127702+Jayasuriya1@users.noreply.github.com>
2022-12-25 18:23:45 +0530

    Add files via upload

---
+++ build/precache-manifest.8e8226eb426fc9fdb2666f474826db79.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e294310d663a793f3cfa43fb8e642b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e9e280f81e60d7880a0",
+    "url": "/static/js/2.450492b5.chunk.js"
+  },
+  {
+    "revision": "f534781e91917b4d7796",
+    "url": "/static/js/main.57f06c09.chunk.js"
+  },
+  {
+    "revision": "325f4b8f32369198a2c5",
+    "url": "/static/js/runtime-main.4e550677.js"
+  }
+]);
